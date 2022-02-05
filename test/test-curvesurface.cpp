3ae3065814009aa013a795c1e78e45147d78f5d3
Rekord <1324596506@qq.com>
2022-02-05 22:04:38 +0800

    test:test the function of curvesurface

--- test/test-curvesurface.cpp
+++ test/test-curvesurface.cpp
@@ -10,28 +10,35 @@ int main(){
 	std::vector<point> ppp2;
 	std::vector<point> ppp3;
 	std::vector<point> pp4;
-	pp1.push_back(point(0,100,0));
-	for(int i=1;i<4;i++)
+	std::vector<point> pp5;
+	pp1.push_back(point(-120,0,0));
+	for(int i=1;i<5;i++)
 		pp1.push_back(point(pp1[i-1].x,pp1[i-1].y,30*i));
 
-	ppp2.push_back(point(40,60,0));
-	for(int i=1;i<4;i++)
+	ppp2.push_back(point(0,150,0));
+	for(int i=1;i<5;i++)
 		ppp2.push_back(point(ppp2[i-1].x,ppp2[i-1].y,30*i));
 
-	ppp3.push_back(point(100,120,0));
-	for(int i=1;i<4;i++)
+	ppp3.push_back(point(250,0,0));
+	for(int i=1;i<5;i++)
 		ppp3.push_back(point(ppp3[i-1].x,ppp3[i-1].y,30*i));
 
-	pp4.push_back(point(120,80,0));
-	for(int i=1;i<4;i++)
+	pp4.push_back(point(0,-110,0));
+	for(int i=1;i<5;i++)
 		pp4.push_back(point(pp4[i-1].x,pp4[i-1].y,30*i));
+	
+	pp5.push_back(point(100,-150,0));
+	for(int i=1;i<5;i++)
+		pp5.push_back(point(pp5[i-1].x,pp5[i-1].y,30*i));
+
 
 	ppp.push_back(pp1);
 	ppp.push_back(ppp2);
 	ppp.push_back(ppp3);
 	ppp.push_back(pp4);
+	ppp.push_back(pp5);
 
-	connectPoints(drawBezierCurveSurface(ppp),0);
+	connectPoints(drawBezierCurveSurface(ppp,1000),0);
 
 
     ege::getch();							//暂停，等待键盘按键
